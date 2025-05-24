87aeed0916428d3c5338e692edfe9e8616d9d2b4
Cen-Jing <whj.simusce@gmail.com>
2025-05-24 10:41:51 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -395,7 +395,7 @@ export default function GlobeDemo() {
   ];
 
   return (
-    <div className="flex flex-row items-center justify-center py-20 h-[100vh] md:h-auto dark:bg-black bg-white relative w-full">
+    <div className="flex flex-row items-center justify-center py-20 h-[100vh] md:h-[100vh] dark:bg-black bg-white relative w-full">
       <div className="max-w-7xl mx-auto w-full relative overflow-hidden h-[100vh] md:h-[100vh] px-4">
         <motion.div
           initial={{
